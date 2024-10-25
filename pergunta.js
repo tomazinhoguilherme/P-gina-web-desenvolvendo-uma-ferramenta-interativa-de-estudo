835e48d77d3c5703f8a9f9a3ebf55b8cafad11bf
tomazinhoguilherme <160505682+tomazinhoguilherme@users.noreply.github.com>
2024-10-25 10:44:06 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
